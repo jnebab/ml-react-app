41865bf75a6f491a49d256a288e297e24bcb9947
John Carl James Nebab <nebab.johncarl@gmail.com>
2020-12-03 00:56:30 +0800

    add classifyImg function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,41 @@
-import "./App.css"
+import React, { useEffect } from "react"
+import Tiger from "./tiger.jpg"
+// import Woman from "./woman.jpg"
+// import Laptop from "./laptop.jpg"
+
+import * as ml5 from "ml5"
 
 function App() {
+  const classifyImg = () => {
+    const classifier = ml5.imageClassifier("MobileNet", modelLoaded)
+
+    function modelLoaded() {
+      console.log("Model loaded!")
+    }
+
+    const image = document.getElementById("image")
+
+    if (classifier) {
+      classifier.predict(image, 5, (err, results) => {
+        console.log({ results })
+      })
+    }
+  }
+
+  useEffect(() => {
+    classifyImg()
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <p>ML with React</p>
+        <img
+          src={Tiger}
+          alt="tiger"
+          id="image"
+          style={{ width: "50%", height: "50%" }}
+        />
       </header>
     </div>
   )
